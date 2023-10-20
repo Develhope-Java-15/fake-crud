74e5864bd38612cbdc0055363669de2a2c24fd00
Sara <saracoppola996@gmail.com>
2023-10-20 14:52:24 +0200

    first commit

---
+++ src/main/java/com/develhope/java15/fakecrud/FakeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.develhope.java15.fakecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/develhope/java15/fakecrud/FakeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develhope.java15.fakecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
