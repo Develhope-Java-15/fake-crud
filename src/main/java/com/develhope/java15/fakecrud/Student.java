4e8e41365bd5b16071e94bbf3d91851c358bbb0c
Alex <alex97av@gmail.com>
2023-10-20 15:45:46 +0200

    add student constructor and getter, add post student insert

--- src/main/java/com/develhope/java15/fakecrud/Student.java
+++ src/main/java/com/develhope/java15/fakecrud/Student.java
@@ -4,4 +4,22 @@ public class Student {
     private int id;
     private String name;
     private float averageGrade;
+
+    public Student(int id, String name, float averageGrade) {
+        this.id = id;
+        this.name = name;
+        this.averageGrade = averageGrade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getAverageGrade() {
+        return averageGrade;
+    }
 }
--- src/main/java/com/develhope/java15/fakecrud/StudentController.java
+++ src/main/java/com/develhope/java15/fakecrud/StudentController.java
@@ -2,10 +2,7 @@ package com.develhope.java15.fakecrud;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +14,14 @@ public class StudentController {
 
     @GetMapping
     @ResponseBody
-    public List<Student> get(){
+    public List<Student> get() {
         return database.retrieveAll();
+    }
+
+    @PostMapping
+    @ResponseBody
+    public Student insert(@RequestBody Student student) {
+        database.insert(student);
+        return student;
     }
 }
